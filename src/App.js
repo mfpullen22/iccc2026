87f2278306c3c51822c3e1353aff241638ed1d68
Matthew Pullen <matthew.pullen@gmail.com>
2023-01-18 19:21:39 -0600

    Added logos, restructed page again

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import crypto from './assets/crypto.jpg';
+//import crypto from './assets/crypto.jpg';
 import logo from './assets/exeter_logo.jpg';
 import campus from './assets/exeter_pic.png';
 import background from './assets/background1.jpg';
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="font-link" style={{
       display: 'flex',
-      flexDirection: 'row',
+      flexDirection: 'column',
       alignItems: 'center',
       justifyContent: 'center',
       height: '100vh',
@@ -18,24 +18,17 @@ function App() {
       color: "#264653"
       
     }}>
-      <div>
+{/*       <div>
       <img src={crypto} alt="crypto" style={{height: "500px"}}/>
+      </div> */}
+
+        <h2>12th International Conference on Cryptococcus & Cryptococcosis </h2>
+        <h2>May/June 2026</h2>
+        <h2>Exeter, UK</h2>
+        <img src={logo} alt="exeter logo" style={{height: "100px", width: "500px"}}/>
+        <img src={campus} alt="exeter campus pic" style={{height: "200px", width: "500px"}}/>
+        <h3>More information to come, be sure to check back periodically!</h3>
       </div>
-      <div className="font-link" style={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      padding: "20px"
-    }}>
-      <h2>12th International Conference on Cryptococcus & Cryptococcosis </h2>
-      <h2>May/June 2026</h2>
-      <h2>Exeter, UK</h2>
-      <img src={logo} alt="exeter logo" style={{height: "100px", width: "500px"}}/>
-      <img src={campus} alt="exeter campus pic" style={{height: "200px", width: "500px"}}/>
-      <h3>More information to come, be sure to check back periodically!</h3>
-      </div>
-    </div>
   );
 }
 
